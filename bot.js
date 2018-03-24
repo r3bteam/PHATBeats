c76ea19725b4be6af9b79e4406fcf5db13463904
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-24 13:50:43 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -424,12 +424,8 @@ async function play(guild, song) {
 async function handleVideo(video, message, playlist = false) {
 	const serverQueue = queue.get(message.guild.id)
 	
-	try {
-		let sendToChannel = message.channel.name === 'bot-commands' ? message.channel : message.guild.channels.findAll('name', 'bot-commands').pop()
-	} catch (error) {
-		let sendToChannel = message.channel
-	}
-
+	let botCommandsChannel = message.guild.channels.find('name', 'bot-commands') ? message.guild.channels.findAll('name', 'bot-commmands').pop() : message.channel
+	
 	const song = {
 		id: Util.escapeMarkdown(video.id),
 		title: video.title,
@@ -437,7 +433,7 @@ async function handleVideo(video, message, playlist = false) {
 		duration: { hours: video.duration.hours, minutes: video.duration.minutes, seconds: video.duration.seconds },
 		url: `https://www.youtube.com/watch?v=${video.id}`,
 		requestedBy: message.author,
-		requestedIn: sendToChannel
+		requestedIn: message.channel.name === 'bot-commands' ? message.channel : botCommandsChannel
 	}
 
 	if (!serverQueue) {

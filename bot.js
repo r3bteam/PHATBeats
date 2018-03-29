029c78f43cbaaaac44b4a656942a51e6fb7e54bc
FatAussieFatBoy <n.barker@hotmail.com.au>
2018-03-29 12:16:15 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -535,7 +535,7 @@ async function play(guild, song) {
 		play(guild, serverQueue.songs[0])
 	}
 	
-	const dispatcher = serverQueue.connection.playStream(ytdl(song.url, {filter: 'audioonly', quality: 94}))
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url, {filter: 'audioonly', quality: 'lowest'}))
 
 	dispatcher.on('end', reason => {
 		serverQueue.songs.shift()
